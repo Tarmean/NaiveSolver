c12a4ecb18312b14b19074fd065c90305b19d514
cyril <cyfah100@hhu.de>
2022-06-06 22:58:33 +0200

    Add decision diagram lattice propagation

--- src/Types.hs
+++ src/Types.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE LambdaCase #-}
 module Types where
 import qualified Data.Set as S
 import Control.Monad
@@ -18,6 +19,8 @@ import Data.Either (partitionEithers)
 import qualified Data.Map.Merge.Lazy as M
 import Data.IntMap.Merge.Strict (SimpleWhenMissing)
 import Data.Maybe (isJust)
+import Control.Applicative
+import Control.Monad.Trans.Maybe
 
 
 
@@ -27,18 +30,16 @@ data DD s
   = If Var (DD s) (DD s)
   | IsTrue
   | IsFalse
-  | And (S.Set (DD s))
-  | Leaf s
+  | And s (S.Set (DD s))
   deriving (Eq, Ord, Show)
 
-type BDD = DD Lit
+type BDD = DD (PMap Var (Val Bool))
 
 data Tag = Absorbing | Neutral
 
 varOf :: IsLit s => DD s -> Var
 varOf (If v _ _) = v
-varOf (And ls) = maximum $ map varOf $ S.toList ls
-varOf (Leaf v) = maxSplitVar v
+varOf (And s ls) = maxSplitVar s `max` (maximum $ (-1:) $ map varOf $ S.toList ls)
 varOf a = (-1)
 
 splitMap :: (a -> (b,c)) -> [a] -> ([b], [c])
@@ -52,10 +53,16 @@ mOr inp = go (inj inp)
     goRec a e'
       | IsTrue `elem` a = IsTrue
       | otherwise = go (M.unionWith (<>) (inj $ filter (/= IsFalse) a) e')
-    -- go e
-    --   | trace ("mOr go " ++ show e) False = undefined
     -- go :: M.Map Var [DD s] -> DD s
     go :: IsLit s => M.Map Var [DD s] -> DD s
+    go e
+      | trace ("mOr go " ++ show e) False = undefined
+    -- FIXME: The And lattice element has to be split here
+    -- But technically it doesn't have ot cause the split?
+    -- Like, one half of the split is always 0
+    -- So maybe:
+    -- - add an operator which specializes the lattice for v=True/v=False? isn't this the same as merging?
+    -- - apply it when needed?
     go e = case M.maxViewWithKey e of
         Nothing -> IsFalse
         Just ((v, ls0), e') -> 
@@ -76,96 +83,122 @@ split :: IsLit s => Var -> DD s -> (DD s, DD s)
 split v (If v' l r)
   | v == v' = (l, r)
   | otherwise = error "illegal split"
-split v (And ls) = (gAnd $ S.fromList $ relL <> invariant, gAnd $ S.fromList $ relR <> invariant)
+split v (And s ls) = (gAndS s $ S.fromList $ relL <> invariant, gAnd $ S.fromList $ relR <> invariant)
   where
+    -- ((sL, sR), se) = case splitLit v s of
+    --    Just o -> (o, mempty)
+    --    Nothing -> (undefined, s)
     (rel,invariant) = partition (\i -> varOf i == v) $ S.toList ls
     (relL, relR) = splitMap (split v) rel
-split v (Leaf a)  | Just o <- splitLit v a = o
 split _ a = (a,a)
 
-testSplit = split 1 $ And (S.fromList [Leaf (L 1 True),Leaf (L 2 True)])
-mAnd :: forall s. (IsLit s, PMonoid (Env s)) => [DD s] -> DD s
+-- testSplit = split 1 $ And (
+mAnd :: forall s. (IsLit s, PMonoid s) => [DD s] -> DD s
 -- mAnd inp
 --   | trace ("mAnd: " ++ show inp) False = undefined
-mAnd inp = evalState (go $ M.fromListWith (<>) (mkTag inp)) pempty
+mAnd inp = flip evalState pempty $ do
+   mo <- runMaybeT (allFlatAnds inp)
+   case mo of
+     Nothing -> pure IsFalse
+     Just (s,o) -> addEnv s <$> go (inj o)
   where
     step ls e' = do
          (done, ls') <- partitionEithers <$> traverse simplifyAnd ls
-         env <- get
-         -- traceM $ "simplified: " ++ show (ls, done, ls', e', env)
-         let ls'' = filter (/=IsTrue) $ flattenAnds ls'
-         if any (== IsFalse) ls''
-          then pure IsFalse
-          else do
-             let q' = M.fromListWith (<>) (mkTag ls'')
-             out <- go $ M.unionWith (<>) e' q'
-             pure $ gAnd (S.fromList $ out : done)
+         runMaybeT (allFlatAnds ls') >>= \case
+           Nothing -> pure IsFalse
+           Just (s, flatLs) -> 
+             let ls'' = filter (/= IsTrue) flatLs
+             in
+             if any (== IsFalse) ls''
+              then pure IsFalse
+              else do
+                 out <- go $ M.unionWith (<>) e' (inj ls'')
+                 pure $ gAndS s (S.fromList $ out : done)
 
     go e
       | M.null e = pure IsTrue
     go e = case M.maxViewWithKey e of
         Nothing -> pure IsTrue
         Just ((v, ls), e') ->  do
-         -- traceM $ "go: " ++ show (v, ls)
+         traceM $ "go: " ++ show (v, ls)
          pre <- get
-         if isJust (checkVar @s v pre)
+         if isJust (evalVar @s v pre)
          then do
-           -- traceM ("skip 1: " ++ show (v, ls))
+           traceM ("skip 1: " ++ show (v, ls))
            step ls e'
          else do
-             -- traceM ("split 1: " ++ show (v, ls))
+             traceM ("split 1: " ++ show (v, ls))
              t <- withEnv @s (isL v) $ step ls e'
-             -- traceM ("split 2: " ++ show (v, ls))
+             traceM ("split 2: " ++ show (v, ls))
              f <- withEnv @s (notL v) $ step ls e'
              pure $ iff v t f
-    flattenAnds = concatMap flattenAnd
-    flattenAnd (And ls) = S.toList ls
-    flattenAnd a = [a]
-    mkTag = map (\i -> (varOf i, [i])) . flattenAnds
 
-withEnv :: IsLit s => s -> State (Env s) a -> State (Env s) a
+    allFlatAnds :: IsLit s => [DD s] -> MaybeT (State s) (s, [DD s])
+    allFlatAnds ls = do
+        env <- get
+        (Just o,s) <- pure (runState (runMaybeT $ traverse flatAnds ls) pempty)
+        env' <- liftMaybe $ env <?> s
+        put env'
+        pure $ (s <-> env, concat o)
+                
+
+    flatAnds :: IsLit s => DD s -> MaybeT (State s) [DD s]
+    flatAnds (And s ls) = do
+       tellEnv s
+       lls <- traverse flatAnds $ S.toList ls
+       pure $ concat lls
+    flatAnds a = pure [a]
+
+    tellEnv s = do
+       env <- get
+       case s <?> env of
+           Nothing -> empty
+           Just s' -> put s'
+
+    inj = M.fromListWith (<>) . map (\i -> (varOf i, [i]))
+
+liftMaybe :: Applicative m => Maybe s -> MaybeT m s
+liftMaybe = MaybeT . pure
+
+withEnv :: IsLit s => s -> State s a -> State s a
 withEnv s m = do
   env <- get
-  o <- case intoEnv s <?> env of
+  o <- case s <?> env of
     Nothing -> error ("Illegal env: " <> show s <> ", " <> show env)
     Just env' -> put env' *> m
   put env
   pure o
 
 
-foo :: DD Lit
+foo :: BDD
 foo =  mAnd [If 2 IsTrue IsFalse, If 1 IsTrue IsFalse]
 
-checkVar :: forall s. IsLit s => Var -> Env s -> Maybe Bool
-checkVar v env = case inEnv env (isL v :: s) of
-    Just IsTrue -> Just True
-    Just IsFalse -> Just False
-    _ -> Nothing
 
-simplify :: forall s. IsLit s => DD s -> State (Env s) (Either (DD s) (DD s))
-simplify (And ls) = do
+simplify :: forall s. IsLit s => DD s -> State s (Either (DD s) (DD s))
+simplify (And s ls) = do
     (a,b) <- partitionEithers <$> (traverse simplify $ S.toList ls)
-    pure $ Right $ gAnd $ S.fromList (a <> b)
+    env <- get
+    pure $ Right $  gAndS (s <-> env) $ S.fromList (a <> b)
 simplify (If v l r) = do
     env <- get
-    case checkVar @s v env of
+    case evalVar @s v env of
         Nothing -> pure $ Right $ If v l r
         Just True -> simplify l
         Just False -> simplify r
 simplify IsTrue = pure $ Right IsTrue
 simplify IsFalse = pure $ Right IsFalse
-simplify a@(Leaf _) = pure $ Right a
-simplifyAnd :: IsLit s => DD s -> State (Env s) (Either (DD s) (DD s))
-simplifyAnd (Leaf l) = do
-  env <- get
-  case inEnv env l of
-    Nothing -> pure $ Right $ IsFalse
-    Just o -> do
-       case intoEnv l <?> env of
-           Nothing -> pure $ Right $ IsFalse
-           Just env' -> do
-             put env'
-             pure $ Right $ o
+-- simplify a@(Leaf _) = pure $ Right a
+simplifyAnd :: IsLit s => DD s -> State s (Either (DD s) (DD s))
+-- simplifyAnd (Leaf l) = do
+--   env <- get
+--   case inEnv env l of
+--     Nothing -> pure $ Right $ IsFalse
+--     Just o -> do
+--        case intoEnv l <?> env of
+--            Nothing -> pure $ Right $ IsFalse
+--            Just env' -> do
+--              put env'
+--              pure $ Right $ o
   -- case env M.!? k of
   --   Nothing -> do
   --     modify $ M.insert k v
@@ -188,35 +221,40 @@ type SolveEnv = M.Map Var Bool
 
 -- mNot :: IsLit s => DD s -> DD s
 mNot (If v t f) = If v (mNot t) (mNot f)
-mNot (And xs) = mOr $ map mNot $ S.toList xs
+mNot (And s xs) = undefined -- mOr $ map mNot $ S.toList xs
 mNot IsTrue = IsFalse
 mNot IsFalse = IsTrue
-mNot (Leaf (L v b)) = Leaf $ L v (not b)
+-- mNot (Leaf (L v b)) = Leaf $ L v (not b)
 
 
 mLit :: Var -> BDD
-mLit v = Leaf $ L v True
+mLit v = And (isL v) mempty
 
 
 
-class (Show (Env s), PSemigroup (Env s), Show s, Ord s) => IsLit s where
-  type Env s
-  type Env s = ()
+class (Show s, RegularSemigroup s, PMonoid s, Show s, Ord s) => IsLit s where
+  -- type Env s
+  -- type Env s = ()
   isL :: Var -> s
   notL :: Var -> s
   maxSplitVar :: s -> Var
   splitLit :: Var -> s -> Maybe (DD s,DD s)
-  intoEnv :: s -> Env s
-  inEnv :: Env s -> s -> Maybe (DD s)
+  evalVar :: Var -> s -> Maybe Bool
+  -- intoEnv :: s -> s
+  -- inEnv :: Env s -> s -> Maybe (DD s)
 class PSemigroup s => SemiLattice s where
     isBot :: s -> Bool
     bot :: s
 class PSemigroup s where
     (<?>) :: s -> s -> Maybe s
-newtype Val a = Val a
+newtype Val a = Val {unVal :: a}
   deriving (Eq, Ord, Show)
 instance Ord a => PSemigroup (Val a) where
     (<?>) (Val a) (Val b) = if a == b then Just (Val a) else Nothing
+class PSemigroup a => InverseSemigroup a  where
+    inv :: a -> a
+class PSemigroup a => RegularSemigroup a  where
+    (<->) :: a -> a -> a
  
 data PMap k v = PMap (M.Map k v)
   deriving (Eq, Ord, Show)
@@ -236,22 +274,34 @@ instance (Ord k, PSemigroup v) => PMonoid (PMap k v) where
 emptyPMap :: PMap k v
 emptyPMap = PMap M.empty
 
-instance IsLit Lit where
-    isL v = L v True
-    notL v = L v False
-    maxSplitVar (L v _) = v
-    splitLit v (L v' b)
-      | v == v' = case b of
-        True -> Just (IsTrue, IsFalse)
-        False -> Just (IsFalse, IsTrue)
-      | otherwise = Nothing
-    type Env Lit = PMap Var (Val Bool)
-    intoEnv (L v b) = PMap $ M.singleton v (Val b)
-    inEnv env (L v b) = case env ?? v of
-       Nothing -> Just $ Leaf (L v b)
-       Just (Val b')
-         | b == b' -> Just $ IsTrue
-         | otherwise -> Just IsFalse
+instance (Ord k, Eq v, PSemigroup v) => RegularSemigroup (PMap k v) where
+    (<->) (PMap m) (PMap m') = PMap $ M.merge M.preserveMissing M.dropMissing (M.zipWithMaybeMatched f) m m'
+      where
+       f _ x y
+         | x == y = Nothing
+         | otherwise = Just x
+        
+instance IsLit (PMap Var (Val Bool)) where
+    isL v = PMap $ M.singleton v (Val True)
+    notL v = PMap $ M.singleton v (Val False)
+    maxSplitVar (PMap p) = case M.maxViewWithKey p of
+      Nothing -> -1
+      Just ((v,_),_) -> v
+    splitLit _ _ = Nothing
+      -- Nothing -> Nothing
+      -- Just (Val True) -> Just (p, PMap $ M.singleton v (Val False))
+      -- | v == v' = case b of
+      --   True -> Just (IsTrue, IsFalse)
+      --   False -> Just (IsFalse, IsTrue)
+      -- | otherwise = Nothing
+    evalVar v env = fmap unVal $ env ?? v
+    -- type Env Lit = PMap Var (Val Bool)
+    -- intoEnv (L v b) = PMap $ M.singleton v (Val b)
+    -- inEnv env (L v b) = case env ?? v of
+    --    Nothing -> Just $ Leaf (L v b)
+    --    Just (Val b')
+    --      | b == b' -> Just $ IsTrue
+    --      | otherwise -> Just IsFalse
 
 (&) :: Ord a => S.Set a -> a -> S.Set a
 a & b = S.insert b a
@@ -263,42 +313,55 @@ iff _ a b | a == b = a
 iff v IsFalse a = litAnd (notL v) a
 iff v a IsFalse = litAnd (isL v) a
 iff v a b
-  | Just o <- cofactor (isL v) a b = o
-  | Just o <- cofactor (notL v) b a = o
-iff v (And ls) (And rs)
-  |  not (S.null inters) = gAnd (inters & iff' v (gAnd (ls S.\\ rs)) (gAnd (rs S.\\ ls)))
-  where inters = S.intersection ls rs
+  | Just o <- cofactor True v a b = o
+  | Just o <- cofactor False v b a = o
+iff v (And sl vl) (And sr vr)
+  |  not (S.null inters)
+   = case sl <?> sr of
+     Nothing -> IsFalse
+     Just o -> gAndS o  (inters & iff' v (gAndS (sl <-> o) (vl S.\\ vr)) (gAndS (sr <-> o)(vr S.\\ vl)))
+  where inters = S.intersection vl vr
 iff v a b = If v a b
 
-cofactor :: s -> (DD s) -> (DD s) -> Maybe (DD s)
--- cofactor v l (And ls)
---   | l `S.member` ls = Just $ gAnd' [l, mOr (S.delete l ls & Leaf v)]
-cofactor _ _ _ = Nothing
+cofactor :: IsLit s => Bool -> Var -> (DD s) -> (DD s) -> Maybe (DD s)
+cofactor b v l (And s ls)
+  | l `S.member` ls = Just $ gAnd' [l,  iff' v b IsTrue (gAndS s $ S.delete l ls)]
+   where
+     iff' v True l r  = iff v l r
+     iff' v False r l = iff v l r
+cofactor _ _ _ _ = Nothing
 
 -- testCofactor = cofactor (isL 3) (Leaf $ isL 1) (gAnd' [Leaf $ isL 1, Leaf $ isL 2])
 gAnd' b = gAnd (S.fromList b)
--- v & x | !v & (x | y)
--- x | (!v & y)
---
--- v & x | !v & (x & y)
--- x & (v | y)
---
---
--- v & x | !v & (w & x | !w & y)
--- v & x | !v & w & x | !v & !w & y
--- x & (v | w) | !v & w & y
+gAndS s b = addEnv s $ gAnd b
+-- 
 
 
+addEnv _ IsFalse = IsFalse
+addEnv s a
+  | s == pempty = a
+addEnv s (And t ls) = case s <?> t of
+    Nothing -> IsFalse
+    Just q -> And q ls
+addEnv s t = And s (S.singleton t)
 
-gAnd :: (Show s, Ord s) => S.Set (DD s) -> DD s
-gAnd ls = case filter (/= top) $ concatMap flatten $ S.toList ls of
-    [] -> top
-    [a] -> a
-    xs
-      | bot `S.member` S.fromList xs -> bot
-    xs -> And (S.fromList xs)
+gAnd :: (IsLit s) => S.Set (DD s) -> DD s
+gAnd ls = 
+  case flattenEnv (S.toList ls) of
+    Nothing -> IsFalse
+    Just env -> 
+       case filter (/= top) $ concatMap flatten $ S.toList ls of
+        [] -> addEnv env top
+        [a] -> addEnv env a
+        xs
+          | bot `S.member` S.fromList xs -> bot
+        xs -> And env (S.fromList xs)
   where
-    flatten (And ls) = S.toList ls
+    flattenEnv :: (IsLit s) => [DD s] -> Maybe s
+    flattenEnv ls = foldl merge1 (Just pempty) [s | And s _ <- ls]
+    merge1 Nothing _ = Nothing
+    merge1 (Just m) s = m <?> s
+    flatten (And _ ls) = S.toList ls
     flatten a = [a]
     bot = IsFalse
     top = IsTrue
@@ -307,9 +370,9 @@ boolToDD :: Bool -> BDD
 boolToDD True = IsTrue
 boolToDD False = IsFalse
 testGroupAnd :: IO ()
-testGroupAnd = quickCheck $ \bs -> And (S.fromList $ map boolToDD bs) == boolToDD (and bs)
+testGroupAnd = quickCheck $ \bs -> And pempty (S.fromList $ map boolToDD bs) == boolToDD (and bs)
 testGroupOr :: IO ()
-testGroupOr = quickCheck $ \bs -> And (S.fromList $ map boolToDD bs) == boolToDD (and bs)
+testGroupOr = quickCheck $ \bs -> And pempty (S.fromList $ map boolToDD bs) == boolToDD (and bs)
   
 iff' v l r = If v l r
 
@@ -320,9 +383,8 @@ iff' v l r = If v l r
 --   | otherwise = group And (S.insert (Leaf l) as)
 -- litOr l a = group Or $ S.fromList [a, Leaf l]
 
-litAnd :: (Show s, Ord s) => s -> DD s -> DD s
-litAnd l (And as) = gAnd (S.insert (Leaf l) as)
-litAnd l a = gAnd $ S.fromList [a, Leaf l]
+litAnd :: (IsLit s) => s -> DD s -> DD s
+litAnd l a = addEnv l a
 
 -- splitOn :: Var -> DD s -> (DD s, DD s)
 -- splitOn v e@(If c t e)
@@ -368,14 +430,16 @@ reduceNaive env (If c t e) =
   else reduceNaive env e
 reduceNaive env IsTrue = True
 reduceNaive env IsFalse = False
-reduceNaive env (And ds) = all (reduceNaive env) ds
-reduceNaive env (Leaf (L v b)) = flipper $ getEnvUnsafe env v
-  where flipper = if b then id else not
+reduceNaive env (And s ds) = case toEnv env <?> s of
+    Just env' -> all (reduceNaive env) ds
+    Nothing -> False
+  where
+    toEnv (a,b,c,d) = PMap $ M.fromList $ zip [0..] (map Val [a,b,c,d])
 toBDDNaive :: BExpr -> BDD
 toBDDNaive (BAnd e1 e2) = mAnd [toBDDNaive e1, toBDDNaive e2]
 toBDDNaive (BOr e1 e2) = mOr $ [toBDDNaive e1, toBDDNaive e2]
-toBDDNaive (BNot (BLit idx)) = Leaf (L (fromEnum idx) False)
-toBDDNaive (BLit idx) = Leaf (L (fromEnum idx) True)
+toBDDNaive (BNot (BLit idx)) = And (notL (fromEnum idx)) mempty
+toBDDNaive (BLit idx) = And (isL (fromEnum idx)) mempty
 toBDDNaive (BNot e) = error $ "Not in NNF " ++ show e
 mkBDD :: BExpr -> BDD
 mkBDD = toBDDNaive . toNNF
